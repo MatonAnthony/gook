46eb100894ea8d4df1c9631d8a06ff4b865ec80c
Anthony Maton <maitre.negi@gmail.com>
2016-04-07 15:18:46 +0200

    Applying google codestyles

--- index.js
+++ index.js
@@ -2,30 +2,36 @@ var config = require('./config.json');
 var gitty = require('gitty');
 var args = require('yargs').argv;
 
-function watch(){
-	config.watch.forEach(function(element, index){
-		var git = gitty(element.directory);
-		if(element.credentials != undefined){
-			git.pull(element.repository, element.branch, element.credentials, function(err){
-				if(err) return console.log(err);
-			});
-		}else{
-			git.pull(element.repository, element.branch, element.username, function(err){
-				if(err) return console.log(err);
-			});
-		};
-	});
+function watch() {
+  config.watch.forEach(function(element, index) {
+    var git = gitty(element.directory);
+    if (element.credentials != undefined) {
+      git.pull(element.repository, element.branch, element.credentials,
+			function(err) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    }else {
+      git.pull(element.repository, element.branch, element.username,
+			function(err) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    };
+  });
 }
 
-if(args.config != undefined){
-	config = require(args.config);
+if (args.config != undefined) {
+  config = require(args.config);
 }
 
-if(config.generic.delay === undefined){
-	throw new Error('Delay missing in configuration file');
+if (config.generic.delay === undefined) {
+  throw new Error('Delay missing in configuration file');
 }
 
 watch();
-setInterval(function(){
-	watch();
+setInterval(function() {
+  watch();
 }, config.generic.delay * 1000);
